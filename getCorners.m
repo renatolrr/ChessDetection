1cf935bf3d4c83cffaddf67690b346535eedbb4c
Eduardo Bonet <e.g.bonet@gmail.com>
2013-02-01 13:59:26 -0500

    Fixed Camera Callibration

---
+++ getCorners.m
@@ -0,0 +1,54 @@
+function corners = getCorners(img,lines)
+    
+    nLines =  length(lines);
+    [bottomLeftCorner, bottomRightCorner] = getBottomCorners(img);
+    
+    distanceThreshold = 25;   
+
+    for k=1:nLines
+        point1 = lines(k).point1;
+        point2 = lines(k).point2;
+        point1distL = sqrt(sum((point1 - bottomLeftCorner) .^ 2));
+        point1distR = sqrt(sum((point1 - bottomRightCorner) .^ 2));
+        point2distL = sqrt(sum((point2 - bottomLeftCorner) .^ 2));
+        point2distR = sqrt(sum((point2 - bottomRightCorner) .^ 2));
+
+        if(point1distL < distanceThreshold && point2distR > distanceThreshold)
+            topLeftCorner = point2;
+        elseif(point1distR < distanceThreshold && point2distL > distanceThreshold)
+            topRightCorner = point2; 
+        elseif(point2distL < distanceThreshold && point1distR > distanceThreshold)
+            topLeftCorner = point1;
+        elseif(point2distR < distanceThreshold && point1distL > distanceThreshold)
+            topRightCorner = point1;
+        end
+    end
+   
+    corners = [bottomRightCorner; topRightCorner; bottomLeftCorner; topLeftCorner];
+    
+end
+
+
+function [leftcentroid, rightcentroid]=getBottomCorners(img)
+
+    hsvImg = rgb2hsv(img);
+
+    leftmask = hsvImg(:,:,1)>0.5 & hsvImg(:,:,1)<0.6;
+    rightmask = hsvImg(:,:,1)>0.1 & hsvImg(:,:,1)<0.2;
+
+    leftcentroid = getCorner(leftmask);
+    rightcentroid = getCorner(rightmask);
+
+
+end
+
+function corner = getCorner(mask)
+
+    mask = imerode(mask,ones(3));
+
+    center = regionprops(mask,'centroid');
+    
+    corner(1) = center(1).Centroid(1);
+    corner(2) = center(1).Centroid(2);
+
+end
--- recoverParrallelLines.m
+++ recoverParrallelLines.m
@@ -1,10 +1,10 @@
-function new_image = recoverParrallelLines(img,points);
+function [new_image,new_corners] = recoverParrallelLines(img,points);
     
     X = points;
     
     A = zeros(9,9);
     
-    p = [0 0; 0 1; 1 0; 1 1]*1024;
+    p = [0 0; 0 1; 1 0; 1 1]*512;
     
     for i=1:4
         A((2*i-1):2*i,1:9) = createRows(X(i,1),X(i,2),p(i,1),p(i,2));
@@ -28,7 +28,19 @@ function new_image = recoverParrallelLines(img,points);
     T = maketform('projective', H');
     
     R = makeresampler({'cubic','cubic'},'fill');
-    new_image = imtransform(img,T);%,R, 'XData', [-600 600], 'YData', [-600 600]);
-
-
+    new_image = imtransform(img,T,R, 'XData', [-20 532], 'YData', [-20 532]);
+    
+    
+    new_corners = p;    
+    
 end
+
+function rows = createRows(x,y,x2,y2)
+
+    rows = [x y 1 0 0 0 -x2*x -x2*y -x2;
+              0 0 0 x y 1 -y2*x -y2*y -y2];
+          
+end
+
+
+
